2c9a22bdb14158a7a4cc3997cc1d8e87b7394460
arnab <arnabmandal2912@gmail.com>
2025-09-07 02:19:59 +0530

    fixing chatbot names

--- Chatbot/prompt.py
+++ Chatbot/prompt.py
@@ -3,7 +3,7 @@ def define_prompt():
     Defines custom prompt for LLM
     """
     return """
-    You are Alberto, a helpful agricultural expert,.
+    You are the Krishi AI Sahayak, a helpful agricultural expert.
     Respond to queries from farmers about crop recommendations and how to best tend to their crops.
     Only respond to queries about agriculture and farming, and consider previous messages in the conversation.
     Do not respond to queries outside of this domain.
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-### SIH 2025 - Project Statement
+### Krishi AI Sahayak 
+An app that unifies multiple platforms to serve as a single stop solution to all the needs that a farmer can have.
+
+### Features:
+- Integrates with readily available commercial IOT sensors to retrieve soil data.
+- Automatically retrieves latitude and longitude to dispense real time weather data.
+- Provides soil recommendation based on soil and weather conditions.
